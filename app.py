e868fe8423677ea674c66b605eb7cdbd0ad65e41
ytono <52069665+ytono@users.noreply.github.com>
2022-02-15 09:18:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from a Github pull request!!!!!!"
+    return "Hello from a Github pull request! now"
 
 
 if __name__ == "__main__":
